d20c0df81b374ce3453775d5628a85833213a5aa
Wenbin Fang <wenbin@cs.wisc.edu>
2011-07-12 20:24:01 -0500

    make agent template

--- user_agent/tmpl/i386-unknown-linux2.4/run.sh
+++
@@ -1,4 +0,0 @@
-export LD_LIBRARY_PATH=`pwd`/../../../$PLATFORM:$LD_LIBRARY_PATH
-#../../../$PLATFORM/Injector $1 `pwd`/lib*.so
-#../../../$PLATFORM/Injector $1 `pwd`/../../../$PLATFORM/libagent.so
-ldd lib*
