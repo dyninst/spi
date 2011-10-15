a668d29a2822872bbb05a552ccd2838cdf1cd416
Wenbin Fang <wenbin@cs.wisc.edu>
2011-10-15 11:42:17 -0500

    checkpoint before experiment

--- user_agent/count/MyAgent.C
+++ user_agent/count/MyAgent.C
@@ -11,28 +11,16 @@ namespace sp {
   extern void report_timer();
 }
 int indent = 0; 
-//sp::SpPayload* payload = NULL;
+
 void print_head(Point* pt) {
-  //sp::payload_start();
-  // SpPayload payload(pt);
-  //SpPayload* payload = payload_mgr(pt);
   callcount++;
-  //sp_print("%d", callcount);
-  //indent++;
-  //int max = 3093050;
-  //if (callcount < (max/100000))
-  //payload->propell();
   sp::propel(pt);
-  //sp::payload_end();
-  //sp::report_timer();
 }
 
 void print_tail(Point* pt) {
-  //indent--;
 }
 
 void segv_handler(int num) {
-  //sp_print("dump maps");
 }
 
 AGENT_INIT
@@ -42,18 +30,8 @@ void MyAgent() {
   sp::SyncEvent::ptr event = sp::SyncEvent::create();
 
   agent->set_parser(parser);
-  //agent->set_init_event(event);
   agent->set_init_head("print_head");
-  // agent->set_init_tail("print_tail");
-  if (getenv("SP_JUMP")) {
-    agent->set_directcall_only(true);
-    agent->set_jump_inst(true);
-    sp_print("Jump-based");
-  } else {
-    agent->set_directcall_only(true);
-    sp_print("Trap-based");
-  }
- // }
+  agent->set_directcall_only(true);
   agent->go();
 }
 
