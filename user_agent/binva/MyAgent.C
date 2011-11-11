53188b306dd82ec4dfe645568d36320a1b89f05c
Wenbin Fang <wenbin@cs.wisc.edu>
2011-11-11 17:09:00 -0600

    add a checker to binva user_agent.

--- user_agent/binva/MyAgent.C
+++ user_agent/binva/MyAgent.C
@@ -4,14 +4,17 @@ using namespace Dyninst;
 using namespace PatchAPI;
 using namespace sp;
 
-
 class BinVaChecker {
   public:
     virtual bool check(SpPoint* pt) = 0;
+    virtual bool post_check(SpPoint* pt) = 0; 
+    virtual bool fini_report() = 0;
 };
 
 class DoubleFreeChecker : public BinVaChecker {
   public:
+    virtual bool fini_report() {}
+    virtual bool post_check(SpPoint* pt) {}
     virtual bool check(SpPoint* pt) {
       PatchFunction* f = sp::callee(pt);
       if (!f) return false;
@@ -33,13 +36,15 @@ class DoubleFreeChecker : public BinVaChecker {
 
 class DangerousFuncChecker : public BinVaChecker {
   public:
+    virtual bool fini_report() {}
+    virtual bool post_check(SpPoint* pt) {}
     virtual bool check(SpPoint* pt) {
       PatchFunction* f = sp::callee(pt);
       if (!f) return false;
 
       bool dangerous = false;
       if (f->name().compare("getwd") == 0 ||
-         f->name().compare("strcpy") == 0)||
+         f->name().compare("strcpy") == 0 ||
          f->name().compare("strlen") == 0) {
 	dangerous = true;
       }
@@ -51,6 +56,8 @@ class DangerousFuncChecker : public BinVaChecker {
 
 class PrintfChecker : public BinVaChecker {
   public:
+    virtual bool fini_report() {}
+    virtual bool post_check(SpPoint* pt) {}
     virtual bool check(SpPoint* pt) {
       PatchFunction* f = sp::callee(pt);
       if (!f) return false;
@@ -62,15 +69,74 @@ class PrintfChecker : public BinVaChecker {
     }
 };
 
+class MallocFreeChecker : public BinVaChecker {
+  public:
+    virtual bool fini_report() {
+      if (MemSet.size() != 0) {
+	sp_print("Unbalanced malloc/free pairs");
+	for (std::set<void*>::iterator i = MemSet.begin(); i != MemSet.end(); i++) {
+	  sp_print("- unfreed pointer: %lx", (*i));
+	}
+      }
+    }
+    virtual bool post_check(SpPoint* pt) {
+      PatchFunction* f = sp::callee(pt);
+      if (!f) return false;
+
+      if (f->name().compare("malloc") == 0) {
+	void* p = (void*)retval(pt);
+	MemSet.insert(p);
+	sp_print("malloc: %lx", p);
+      }
+    }
+    virtual bool check(SpPoint* pt) {
+      PatchFunction* f = sp::callee(pt);
+      if (!f) return false;
+      if (f->name().compare("free") == 0) {
+	ArgumentHandle h;
+	void** p = (void**)pop_argument(pt, &h, sizeof(void*));
+	if (MemSet.find(*p) == MemSet.end()) {
+	  sp_print("freeing an unmalloced pointer: %lx", *p);
+	} else {
+	  MemSet.erase(*p);
+	  // sp_print("free: %lx", *p);
+	}
+      }
+    }
+  protected:
+    static std::set<void*> MemSet;
+};
+std::set<void*> MallocFreeChecker::MemSet;
+
+class FileAccessChecker : public BinVaChecker {
+  public:
+    virtual bool fini_report() {}
+    virtual bool post_check(SpPoint* pt) {}
+    virtual bool check(SpPoint* pt) {
+      PatchFunction* f = sp::callee(pt);
+      if (!f) return false;
+      if (f->name().compare("open") == 0) {
+	ArgumentHandle h;
+	char** fn = (char**)pop_argument(pt, &h, sizeof(void*));
+	sp_print("Open: %s", *fn);
+      }
+    }
+};
+
 class BinVa {
   public:
     BinVa() {
+      /*
       checkers_.insert(new DoubleFreeChecker);
       checkers_.insert(new DangerousFuncChecker);
       checkers_.insert(new PrintfChecker);
+      checkers_.insert(new MallocFreeChecker);
+      */
+      checkers_.insert(new FileAccessChecker);
     }
     ~BinVa() {
       for (std::set<BinVaChecker*>::iterator i = checkers_.begin(); i != checkers_.end(); i++) {
+	(*i)->fini_report();
 	delete *i;
       }
     }
@@ -79,21 +145,24 @@ class BinVa {
 	(*i)->check(pt);
       }
     }
+    void post_run(SpPoint* pt) {
+      for (std::set<BinVaChecker*>::iterator i = checkers_.begin(); i != checkers_.end(); i++) {
+	(*i)->post_check(pt);
+      }
+    }
   private:
     std::set<BinVaChecker*> checkers_; 
 };
 
 BinVa bv;
 void binva_head(SpPoint* pt) {
-
   bv.run(pt);
-
   sp::propel(pt);
 }
 
 void binva_tail(SpPoint* pt) {
-  PatchFunction* f = sp::callee(pt);
-  if (!f) return;
+  bv.post_run(pt);
+  sp::propel(pt);
 }
 
 AGENT_INIT
@@ -103,7 +172,7 @@ void MyAgent() {
   agent->set_init_event(event);
 
   agent->set_init_head("binva_head");
-  // agent->set_init_tail("binva_tail");
+  agent->set_init_tail("binva_tail");
 
   agent->go();
 }
