1dd8d5e71a41beb104f6475776dd376559d4b6ac
Wenbin Fang <wenbin@cs.wisc.edu>
2011-11-18 09:27:38 -0600

    modify user_agent/tmpl

--- user_agent/tmpl/MyAgent.C
+++ user_agent/tmpl/MyAgent.C
@@ -7,32 +7,72 @@ using namespace sp;
 int indent = 0;
 int callcount = 0;
 
-void print_head(SpPoint* pt) {
+//--------------------------------------------
+// This is invoked before every function call
+//--------------------------------------------
+void print_before(SpPoint* pt) {
 
-if (pt->tailcall()) {
+  //---------------------------------------------
+  // For tail call, only print_before is invoked,
+  // and ignore print_after
+  //---------------------------------------------
+  if (pt->tailcall()) {
     sp::propel(pt);
     return;
   }
 
+  //---------------------------------------------
+  // Get callee's PatchFunction instance
+  //---------------------------------------------
   PatchFunction* f = sp::callee(pt);
   if (!f) return;
+
+  //---------------------------------------------
+  // Get callee's name
+  //---------------------------------------------
   string callee_name = f->name();
+
+  //---------------------------------------------
+  // Count the number of invocations
+  //---------------------------------------------
   callcount ++;
 
+  //---------------------------------------------
+  // Control the indentation, and print out call
+  // graph
+  //---------------------------------------------
   string fmt;
   for (int i = 0; i < indent; i++)
     fmt += ' ';
   sp_print("%sEnter %s %d @ %lx", fmt.c_str(), callee_name.c_str(), callcount, pt->block()->last());
   ++indent;
 
+  //------------------------------------------
+  // Propel to next point (next function call
+  //------------------------------------------
   sp::propel(pt);
 }
 
-void print_tail(Point* pt) {
+//--------------------------------------------
+// This is invoked after every function call
+//--------------------------------------------
+void print_after(Point* pt) {
+
+  //---------------------------------------------
+  // Get callee's PatchFunction instance
+  //---------------------------------------------
   PatchFunction* f = sp::callee(pt);
   if (!f) return;
+
+  //---------------------------------------------
+  // Get callee's name
+  //---------------------------------------------
   string callee_name = f->name();
 
+  //---------------------------------------------
+  // Control the indentation, and print out call
+  // graph
+  //---------------------------------------------
   --indent;
   string fmt;
   for (int i = 0; i < indent; i++)
@@ -43,16 +83,12 @@ void print_tail(Point* pt) {
 AGENT_INIT
 void MyAgent() {
   sp::SpAgent::ptr agent = sp::SpAgent::create();
-
   sp::SpParser::ptr parser = sp::SpParser::create();
-  agent->set_parser(parser);
-
   sp::SyncEvent::ptr event = sp::SyncEvent::create();
+
+  agent->set_parser(parser);
   agent->set_init_event(event);
-
-  agent->set_init_head("print_head");
-  agent->set_init_tail("print_tail");
-
-  //agent->set_directcall_only(true);
+  agent->set_init_head("print_before");
+  agent->set_init_tail("print_after");
   agent->go();
 }
--- user_agent/tmpl/i386-unknown-linux2.4/preload.sh
+++
@@ -1,8 +0,0 @@
-SP=/afs/cs.wisc.edu/p/paradyn/development/wenbin/spi
-BIN=/scratch/wenbin/binaries/bin/bin
-
-source ../make.config
-export LD_LIBRARY_PATH=$SP_DIR/$PLATFORM:$LD_LIBRARY_PATH
-
-#export SP_DEBUG=0
-LD_PRELOAD="./$AGENT_NAME" $BIN/ls
